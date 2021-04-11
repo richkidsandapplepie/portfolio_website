a04fb14dbc1979b64363b473774c4b5d3b264316
Richard Kwon <richardhkwon@gmail.com>
2021-04-11 16:05:16 -0500

    dynamic recent project page

--- src/components/recent_projects.js
+++ src/components/recent_projects.js
@@ -1,36 +1,65 @@
-import React from 'react';
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
 import '../css/recent.css';
 
-function RecentProjects() {
-  return (
-    <>
-      <div className="row">
-        <div className="col-sm-4">
-          <div className="section">
-            <div className="img-container">
-              <img src="../images/img_1.jpg" className="card-image card-img-top" alt="..."></img>
+const PROJECTS = [
+  {
+    title: "moviebuff",
+    overview: "movie website",
+    image: "./images_1.jpg"
+  },
+  {
+    title: "tallyup",
+    overview: "game night managing app",
+    image: "./images_1.jpg"
+  },
+  {
+    title: "what's your fitness",
+    overview: "fitness calculator",
+    image: "./images_1.jpg"
+  }
+];
+
+class RecentProjects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: PROJECTS,
+      selectedProject: null
+    }
+  }
+
+  renderSelectedProject(project) {
+    if (project) {
+      return (<div>success</div>);
+    } else {
+      return (<div>select a project</div>);
+    }
+  }
+
+  selectProject = (title) => {
+    const project = this.state.projects.filter(item => item.title === title)[0];
+    this.setState({ selectedProject: project });
+    console.log(this.state.selectedProject);
+  }
+
+  render() {
+    return (
+      <>
+        <div className="container list-container">
+          <div className="row">
+            <div className="col-md-3 list">
+              <div className="list-item" onClick={() => this.selectProject('moviebuff')}>moviebuff</div>
             </div>
-            <div className="card-title"><strong>TallyUp</strong></div>
-            <div className="card-info">Game night manager</div>
+            <div className="col-md-9">
+              {this.renderSelectedProject(this.state.selectedProject)}
+            </div>
           </div>
         </div>
-        <div className="col-sm-4">
-          <div className="section">
-            <img src="../images/img_1.jpg" className="card-image card-img-top" alt="..."></img>
-            <div className="card-title"><strong>TallyUp</strong></div>
-            <div className="card-info">Game night manager</div>
-          </div>
-        </div>
-        <div className="col-sm-4">
-          <div className="section">
-            <img src="../images/img_1.jpg" className="card-image card-img-top" alt="..."></img>
-            <div className="card-title"><strong>TallyUp</strong></div>
-            <div className="card-info">Game night manager</div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
+
 }
 
 export default RecentProjects;
