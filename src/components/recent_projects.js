f84f60e4dcbafecec55204e78416b7c1997d3d0e
Richard Kwon <richardhkwon@gmail.com>
2021-05-15 20:02:21 -0500

    Card edits

--- src/components/recent_projects.js
+++ src/components/recent_projects.js
@@ -5,21 +5,24 @@ import { Fade, Stagger } from 'react-animation-components';
 const PROJECTS = [
   {
     id: 0,
-    title: "moviebuff",
+    title: "minimoviebuff",
     overview: "movie website",
-    image: "./images/tallyup_logo.png"
+    image: "./images/tallyup_logo.png",
+    link: "https://play.google.com/store/apps/details?id=io.cazjapp.tallyup"
   },
   {
     id: 1,
     title: "tallyup",
     overview: "game night managing app",
-    image: "./images/tallyup_logo.png"
+    image: "./images/tallyup_logo.png",
+    link: "https://play.google.com/store/apps/details?id=io.cazjapp.tallyup"
   },
   {
     id: 2,
     title: "whatsyourfitness",
     overview: "fitness calculator",
-    image: "./images/wyf_logo.jpg"
+    image: "./images/wyf_logo.jpg",
+    link: "https://play.google.com/store/apps/details?id=io.richardhkwon.fitness_calculator"
   }
 ];
 
@@ -30,57 +33,7 @@ class RecentProjects extends Component {
       projects: PROJECTS,
     }
   }
-
-  renderSelectedProject(project) {
-    if (project) {
-      return (
-        <>
-          <div key={project.id} className="col-md-3">
-            <img className="projectImg project-card" src={project.image}></img>
-          </div>
-          <div className="col-md-3">
-            {project.title}
-            <p>Stack: </p>
-            <p>{project.overview}</p>
-            <div className="link-button">go to website</div>
-          </div>
-        </>
-      );
-    } else {
-      return (<div>select a project</div>);
-    }
-  }
-
-  renderProjects() {
-    const projects = this.state.projects;
-    const projectList = projects.map(project => {
-      return (
-        <>
-          <Fade in>
-            <div className="row">
-              <div key={project.id} className="col-md-3">
-                <img className="projectImg project-card" src={project.image}></img>
-              </div>
-              <div className="col-md-3">
-                {project.title}
-                <p>Stack: </p>
-                <p>{project.overview}</p>
-                <div className="link-button">go to website</div>
-              </div>
-            </div>
-          </Fade>
-        </>
-      );
-    });
-    return projectList;
-  }
-
-  selectProject = (title) => {
-    const project = this.state.projects.filter(item => item.title === title)[0];
-    this.setState({ selectedProject: project });
-    console.log(this.state.selectedProject);
-  }
-
+  
   render() {
     return (
       <>
@@ -98,10 +51,11 @@ class RecentProjects extends Component {
                           <img className="projectImg" src={project.image}></img>
                         </div>
                         <div class="col-md-6">
-                          <a className="title">{project.title}</a>
+                          <a href={project.link} target="_blank" 
+                          className="title">{project.title}</a>
                           <hr></hr>
-                          <p>Stack: </p>
-                          <p>{project.overview}</p>
+                          <p className="details"><strong>Stack: </strong></p>
+                          <p className="details">{project.overview}</p>
                         </div>
                       </div>
                     </div>
