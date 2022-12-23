7cf5fe92325b83c50f558b50232791dac694e25f
Richard <richardhkwon@gmail.com>
2022-12-23 15:22:07 -0600

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>Rich Kwon Portfolio</h1>
+    <p>
+      Under construction...
+    </p>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+   
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+</style>
